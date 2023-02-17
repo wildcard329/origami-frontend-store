c8ed3b720f6577f2a9a11362ec0f31917c2950f5
redsun62 david <adam.david8296@gmail.com>
2023-02-16 20:29:59 -0800

    [cart-002] added context for modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import AppLayout from './AppLayout';
 import AppRouter from './AppRouter';
 import { CartProvider } from './utils/contexts/cartContext';
 import { MessageProvider } from './utils/contexts/messageContext';
+import { ModalProvider } from './utils/contexts/modalContext';
 
 const App = () => 
   <CartProvider>
     <MessageProvider>
-      <AppLayout>
-        <AppRouter />
-      </AppLayout>
+      <ModalProvider>
+        <AppLayout>
+          <AppRouter />
+        </AppLayout>
+      </ModalProvider>
     </MessageProvider>
   </CartProvider>
 
