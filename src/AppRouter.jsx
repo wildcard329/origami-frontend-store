4ab2138b0682972f634dcf71972945ec5de6bcc1
redsun62 david <adam.david8296@gmail.com>
2023-02-20 11:37:48 -0800

    [code-split-001] implemented lazy loading in app router

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Checkout from './pages/Checkout';
-import Home from './pages/Home';
-import Product from './pages/Product';
-import Products from './pages/Products';
+// import Checkout from './pages/Checkout';
+// import Home from './pages/Home';
+// import Product from './pages/Product';
+// import Products from './pages/Products';
+
+const FallBackRender = ({ children }) => 
+  <Suspense fallback={<div>loading...</div>}>
+    {children}
+  </Suspense>
+
+const Checkout = lazy(() => import('./pages/Checkout.jsx'));
+const Home = lazy(() => import('./pages/Home.jsx'));
+const Product = lazy(() => import('./pages/Product.jsx'));
+const Products = lazy(() => import('./pages/Products.jsx'))
 
 const AppRouter = () =>
   <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/origami/:id' element={<Product />} />
-    <Route path='/origami' element={<Products />} />
-    <Route path='/checkout' element={<Checkout />} />
+    <Route path='/' element={<FallBackRender><Home /></FallBackRender>} />
+    <Route path='/origami/:id' element={<FallBackRender><Product /></FallBackRender>} />
+    <Route path='/origami' element={<FallBackRender><Products /></FallBackRender>} />
+    <Route path='/checkout' element={<FallBackRender><Checkout /></FallBackRender>} />
   </Routes>
 
 export default AppRouter;
