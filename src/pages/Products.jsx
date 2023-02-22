1440de1fcf737dcc7d49380fafd4a745cb21689a
redsun62 david <adam.david8296@gmail.com>
2023-02-22 15:47:46 -0800

    [assets-007] added assets for books and revisited logic for fetching data

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { getProducts } from '../controllers/productController';
+import { useLocation } from 'react-router-dom';
+import { getProductsByCategory } from '../controllers/productController';
 import PosterCard from '../components/templates/PosterCard.jsx';
 import "./products.css";
 
 const Products = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const { pathname } = useLocation();
+  const category = pathname.substring(1, pathname.length)
 
   useEffect(() => {
     // This is currently pulling from a json file, but will pull from an api
-    setProducts(getProducts());
-  }, [])
+    setProducts(getProductsByCategory(category));
+  }, [category])
 
   return(
     <div className="product-grid">
