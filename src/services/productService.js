9499bbb7338ad589ab0d8ef1adfca866cc987642
wildcard329 <enkiduwildman@gmail.com>
2023-02-23 08:50:25 -0800

    [services] abstracted service methods to array methods

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,13 +1,13 @@
-import { getUniqueKeys } from '../utils/functions/arrayMethods.js';
+import { getUniqueKeys, filterArray, findArrayObject } from '../utils/functions/arrayMethods.js';
 import origami from '../assets/configs/productConfig.json';
 import books from '../assets/configs/productConfig2.json';
 const products = [...origami, ...books ];
 
+// most of these methods will communicate to an api, but for now they're just using config mocks.
 export const getProductSubcategories = (products) => getUniqueKeys(products, 'category');
 
 export const getProductsByCategory = (category) => category === 'origami' ? origami : category === 'books' ? books : null; 
 
-export const getProductsBySubcategory = (subcategory) => products.filter((product) => product.category === subcategory);
+export const getProductsBySubcategory = (subcategory) => filterArray(products, 'category', subcategory);
 
-export const getProductById = (id) => products.find((product) => product.id === id);
-
+export const getProductById = (id) => findArrayObject(products, 'id', id);
--- src/utils/functions/arrayMethods.js
+++ src/utils/functions/arrayMethods.js
@@ -4,3 +4,7 @@ export const reduceArray = (arrVals, arrKey, initVal) => arrVals.reduce(
 
 export const getUniqueKeys = (arrVals, objKey) => 
   [... new Set(arrVals.map((item) => item[objKey]))]
+
+export const filterArray = (arrVals, filterKey, filterCondition) => arrVals.filter((item) => item[filterKey] === filterCondition);
+
+export const findArrayObject = (arrVals, filterKey, filterCondition) => arrVals.find((item) => item[filterKey] === filterCondition);
