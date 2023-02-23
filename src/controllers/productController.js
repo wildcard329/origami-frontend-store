95d965ecf6a7642b55ddea3ddab074934d41f378
wildcard329 <enkiduwildman@gmail.com>
2023-02-22 18:00:48 -0800

    [shop-sort-001] added filter feature for product sub categories

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,12 @@
+import { getUniqueKeys } from '../utils/functions/arrayMethods.js';
 import origami from '../assets/configs/productConfig.json';
 import books from '../assets/configs/productConfig2.json';
-const products = [...origami, ...books]
+const products = [...origami, ...books];
+
+export const getProductSubcategories = (products) => getUniqueKeys(products, 'category');
 
 export const getProductsByCategory = (category) => category === 'origami' ? origami : category === 'books' ? books : null; 
+
+export const getProductsBySubcategory = (subcategory) => products.filter((product) => product.category === subcategory);
 
 export const getProductById = (id) => products.find((product) => product.id === id);
--- src/utils/functions/arrayMethods.js
+++ src/utils/functions/arrayMethods.js
@@ -2,3 +2,5 @@ export const reduceArray = (arrVals, arrKey, initVal) => arrVals.reduce(
     (accumulator, cur) =>  accumulator + cur[arrKey], initVal
   );
 
+export const getUniqueKeys = (arrVals, objKey) => 
+  [... new Set(arrVals.map((item) => item[objKey]))]
